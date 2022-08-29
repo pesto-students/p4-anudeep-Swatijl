508ca08f63ea7d9e1f7ff0d399355fce26b6a876
Swatijl <swatisam76@gmail.com>
2022-08-29 12:41:56 +0530

    resolved set question

--- Week-5/set.js
+++ Week-5/set.js
@@ -1,4 +1,14 @@
 
-const hasDuplicate = (arr) => new Set(arr).size !== arr.length;
+const hasDuplicate = (arr) => {
+    let set =  new Set();
+    let ar = []
+    for (let a of arr){
+        if (set.has(a)){
+            ar.push(a)
+        }
+        set.add(a)
+    }
+    return ar
+} 
 
-console.log(hasDuplicate([1, 5, -1, 4, 1])); 
+console.log(hasDuplicate([1, 5, -1, 4, 1, 5])); 
